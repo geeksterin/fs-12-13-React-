12585978e59b564dbddd5aff56081d5fa78c717e
Divyansh <divyanshmoonat@outlook.com>
2023-12-15 22:06:25 +0530

    12-12-2023|useState

--- src/App.js
+++ src/App.js
@@ -1,64 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Content from "./Components/Content";
-import ColorCard from "./Components/ColorCard";
+
+import Counter from "./Components/12-12-2023/Counter";
 
 function App() {
-  let userName = "Mark";
   return (
     <div className="App">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexWrap: "wrap",
-        }}
-      >
- 
-        <ColorCard colorCode="#FF6663" colorName="PINK" />
-        <ColorCard colorCode="#ffffff" colorName="White" />
-        <ColorCard colorCode="#38ba15" colorName="Green" />
-        <ColorCard colorCode="#fe7e00" colorName="Orange" />
-        <ColorCard colorCode="#303020" colorName="Dark Blue" />
-      </div>
-      {/* <Content
-        userName="Divyansh"
-        mobNo="9999999999"
-        age={30}
-        hasDrivingLicense={true}
-        // marks={[90, 75, 70]}
-        marks={[
-          {
-            science: 90,
-          },
-          {
-            mathematics: 85,
-          },
-        ]}
-        completeAddress={{
-          addressLine1: "123 ABC Colony",
-          addressLine2: "Near XYZ Place",
-          city: "Indore",
-          state: "Madhya Pradesh",
-          country: "India",
-        }}
-      /> */}
-      {/* <Content
-        userName="John"
-        address="456, XYZ Colony"
-        mobNo="1111111111"
-        age={40}
-        hasDrivingLicense={false}
-      />
-      <Content
-        userName="Peter"
-        address="789, ASDF Colony"
-        mobNo="5555555555"
-        age={40}
-        hasDrivingLicense={false}
-      /> */}
+      <Counter />
     </div>
   );
 }
---
+++ src/Components/12-12-2023/Counter.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+const Counter = () => {
+  //   let counter = 0;
+  // Syntax
+  // let [getterVariableName, setterFunction] = useState(initial-value);
+
+  let [counter, setCounter] = useState(0); // declaration
+  let [userName, setUserName] = useState("John");
+  let [student, setStudent] = useState({
+    rollNo: 123,
+    name: "ABC",
+    bloodGroup: "A+",
+  });
+
+  //   counter = 50; // COMPLETELY AVOID THIS
+
+  const onCounterDecrease = () => {
+    // console.log("Decrease button clicked");
+    const updatedValue = counter - 1;
+    setCounter(updatedValue);
+  };
+
+  const onCounterIncrease = () => {
+    // console.log("Increase button clicked");
+    const updatedValue = counter + 1;
+    setCounter(updatedValue);
+  };
+
+  const onUserNameUpdate = () => {
+    console.log("User name update btn clicked");
+    let updatedName = "John";
+    if (userName === "John") {
+      updatedName = "Peter";
+    }
+    setUserName(updatedName);
+  };
+
+  const onStudentNameUpdate = () => {
+    const updateObject = { ...student, name: "XYZ" };
+
+    setStudent(updateObject);
+  };
+
+  return (
+    <div>
+      <h1>Counter Component</h1>
+      <h1>Exp 1</h1>
+      <div style={{ fontSize: "30px" }}>{counter}</div>
+      <div>
+        <button id="decBtn" onClick={onCounterDecrease}>
+          Decrease
+        </button>
+        <button id="incBtn" onClick={onCounterIncrease}>
+          Increase
+        </button>
+        <hr />
+        <h1>Exp 2</h1>
+        Name : {userName}
+        <br />
+        <button onClick={onUserNameUpdate}>Toggle Name</button>
+        <hr />
+        <h1>Exp 3</h1>
+        <h2>Student Details</h2>
+        Name = {student.name} <br />
+        Blood Group = {student.bloodGroup}
+        <button onClick={onStudentNameUpdate}>Update Student Name</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
