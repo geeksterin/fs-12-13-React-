007d1cb229b05168cdf14a8f670237dd2706c70c
Divyansh <divyanshmoonat@outlook.com>
2024-01-15 22:01:30 +0530

    15-01-2024 | Revision

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import "./App.css";
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createHashRouter } from "react-router-dom";
 
 import ContextDemo from "./Components/11-01-2024/ContextDemo";
 import { UserContext } from "./context/UserContext";
+import HotelDetails from "./Components/08-01-2024/HotelDetails";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <ContextDemo />,
+      errorElement: <h1>Error element screen for not found</h1>,
     },
+    {
+      path: "/about-us",
+      element: <h1>About us page</h1>,
+    },
+    {
+      path: "/hotel/:hotelId",
+      element: <HotelDetails />
+    }
+    // {
+    //   path: "*",
+    //   element: <h1>Page not found</h1>,
+    // },
   ]);
 
   const [user, setUser] = useState({
